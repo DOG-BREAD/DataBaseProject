f6fe8103d2c42c5b4761f0b6eceb379f4697c4ec
DOG-BREAD <samueljacobsbuisness@gmail.com>
2023-04-21 23:55:32 -0500

    y

--- website/databse/ror2/ror2/settings.py
+++ website/databse/ror2/ror2/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'ror2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'riskofrain2',
+        'USER': 'newuser',
+        'PASSWORD': 'pa$$word',
+        'HOST': '192.168.192.72',
+        'PORT': '3306',
     }
 }
 
--- website/databse/ror2/ror2app/views.py
+++ website/databse/ror2/ror2app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from .models import Items
 
 # Create your views here.
 def index(request):
-    return render(request, "index.html")
+    data= Items.objects.all()
+    return render(request, "index.html", {'data' : data} )
+
